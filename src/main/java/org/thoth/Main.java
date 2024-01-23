fddc2b0e7d7a056c46ca6342a0efa5cde891f913
g0rk4 <gorka.chaudouin@gmail.com>
2024-01-23 11:53:55 +0100

    First commit

---
+++ src/main/java/org/thoth/Main.java
@@ -0,0 +1,10 @@
+package org.thoth;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        logger.info("Starting chat server application");
+    }
+}
---
+++ src/main/java/org/thoth/server/server.java
@@ -0,0 +1,5 @@
+package org.thoth.server;
+
+public class server {
+
+}
